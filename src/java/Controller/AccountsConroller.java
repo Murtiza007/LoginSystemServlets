ba38b94d7c0fb6bcbb014257c25ad33d4a9da4ec
Murtiza007 <murtizakhirsheed123@gmail.com>
2025-03-06 11:37:25 +0530

    second commit

---
+++ src/java/Controller/AccountsConroller.java
@@ -0,0 +1,63 @@
+
+package Controller;
+
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.users;
+
+
+public class AccountsConroller {
+    
+   
+    
+    public void login(users user) throws IOException{
+     
+        if(isValid(user))
+        {
+           if(isAuthentic(user))
+           {
+               authorize(user);
+               user.setMessage("welcome");
+           }
+           else{
+               user.setMessage("Incorrect Credentials");
+           }
+        }
+        else{
+            user.setMessage("Please enter all fields");
+        }
+        
+    
+    }
+    
+    
+    public boolean isValid(users user){
+    
+        return (!user.getUsername().equals("")&&
+                !user.getPassword().equals(""));
+    
+    }
+    public boolean isAuthentic(users user){
+        return (user.getUsername().equals("moin")&&
+                user.getPassword().equals("123"));
+    
+    }
+    public void authorize(users user) throws IOException{
+       
+        HttpSession session=user.getReq().getSession();
+        session.setAttribute("AuthorizationSession", user.getUsername());
+        user.getRes().sendRedirect("profile");
+    }
+    public void isAuthorised(users user) throws IOException{
+    HttpSession session=user.getReq().getSession();
+        if(session.getAttribute("AuthorizationSession")==null
+                || session.getAttribute("AuthorizationSession").equals("")){
+            user.getRes().sendRedirect("home?ID=101");
+        
+        }
+    
+    }
+    
+    
+    
+}
